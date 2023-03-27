d18c18c262adfdecc1043b246cecc9ea363a918d
Tapwatero <charliewoolf20@gmail.com>
2023-03-27 21:51:45 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const {WebSocketServer} = require("ws");
+
+const wss = new WebSocketServer({port: 8080});
+
+wss.on('connection', ws => {
+    console.log('WebSocket Connected!');
+
+    ws.on('message', packet => {
+        packet = JSON.parse(packet);
+
+        // Send the received message back to the client
+
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === 1) {
+                client.send(`[${packet.username}]: ${packet.message}`);
+            }
+        });
+
+        ws.on('close', () => {
+            console.log('WebSocket Disconnected!');
+        });
+    });
+});
